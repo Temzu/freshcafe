c473172f7c8492bc009e08734c1f390c6c00f705
Temzu <40913z@mail.ru>
2022-11-17 15:19:17 +0300

    Initial commit

---
+++ src/test/java/com/temzu/freshcafe/FreshcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temzu.freshcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshcafeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
